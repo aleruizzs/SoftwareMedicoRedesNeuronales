9b54c46314736bfc150a090d7a9b30ca0c92aa90
Alejandro Ruiz <alejandroruizsalazar21@gmail.com>
2025-06-06 10:54:16 +0200

    Quitado modelo de soporte "torax"

--- inference/model_strategies/factory.py
+++ inference/model_strategies/factory.py
@@ -2,9 +2,6 @@ def get_model_strategy(model_name: str):
     if model_name == "columna":
         from .columna import ColumnaStrategy
         return ColumnaStrategy()
-    elif model_name == "torax":
-        from .torax import ToraxStrategy
-        return ToraxStrategy()
     elif model_name == "fake":
         from .fake import FakeStrategy
         return FakeStrategy()
